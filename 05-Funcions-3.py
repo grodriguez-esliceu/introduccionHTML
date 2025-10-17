dcffbc9f91d87d8fab23d7da135de3279de0c30e
gabriel <grodriguezh>
2025-10-17 12:02:45 +0200

    hemos creado una página de bienvenida

---
+++ 05-Funcions-3.py
@@ -0,0 +1,14 @@
+def info(nom):
+    return nom
+estudiant=info("Tomeu")
+print(estudiant)
+
+
+def saludar(nom):
+    return "Hola "+ nom +""
+missatge=saludar("Pep")
+print(missatge)
+
+def jugador(nom, edat):
+    return "En "+ nom +" té "+ edat + " i juga al Reus"
+equip=jugador("Pep", "23")
---
+++ 05.Funcions-2.py
@@ -0,0 +1,9 @@
+def info(nom, prof):
+    print(f"Sòc {nom} i a més sòc {prof}")
+
+info("Spiderman", "Hacker")
+info("Batman", "Jardiner")
+info("Thor", "Cuiner")
+
+def info(nom, prof = "Desconeguda"):
+    print(f"s")
