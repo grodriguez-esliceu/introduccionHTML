08a1a48c5f6361081f81e9526b2e608c78bff77c
gabriel <grodriguezh>
2025-10-18 15:50:32 +0200

    borrar

--- 05.Funcions-2.py
+++
@@ -1,9 +0,0 @@
-def info(nom, prof):
-    print(f"Sòc {nom} i a més sòc {prof}")
-
-info("Spiderman", "Hacker")
-info("Batman", "Jardiner")
-info("Thor", "Cuiner")
-
-def info(nom, prof = "Desconeguda"):
-    print(f"s")
